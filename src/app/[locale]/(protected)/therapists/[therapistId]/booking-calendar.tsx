a7f53726635b8757c5e6cdcb29cfd06dcd55eee7
Arthur Berg <radarberg@gmail.com>
2024-07-18 10:24:06 +0200

    skip removing calendar when date is selected in therapist calendar

--- src/app/[locale]/(protected)/therapists/[therapistId]/booking-calendar.tsx
+++ src/app/[locale]/(protected)/therapists/[therapistId]/booking-calendar.tsx
@@ -101,7 +101,26 @@ const BookingCalendar = ({
           </div>
           {date.justDate ? (
             <>
-              <div className="flex gap-4 flex-wrap">
+              <Calendar
+                mode="single"
+                selected={date.justDate ? date.justDate : today}
+                onSelect={(date) => {
+                  setDate((prev) => ({
+                    ...prev,
+                    justDate: date ? date : today,
+                  }));
+                }}
+                className="rounded-md border h-full w-full flex"
+                classNames={{
+                  months:
+                    "flex w-full flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 flex-1",
+                  month: "space-y-4 w-full flex flex-col",
+                  table: "w-full h-full border-collapse space-y-1",
+                  head_row: "",
+                  row: "w-full mt-2",
+                }}
+              />
+              <div className="flex gap-4 flex-wrap mt-2">
                 {times?.map((time, i) => (
                   <div key={`time-${i}`} className="rounded-sm bg-gray-100 p-2">
                     <Button
