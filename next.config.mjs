5f9cb4f2d137a08970ec045c7f4d6672fd75143b
Arthur Berg <radarberg@gmail.com>
2024-09-04 14:24:11 +0200

    fixed build issues

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {experimental: {serverComponentsExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner']}};
 
 export default withNextIntl(nextConfig)
 
