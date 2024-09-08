5a000f96d8944cfb9e4a417efff7484b574a6510
Arthur Berg <arthur@Arthurs-MacBook-Pro.local>
2024-09-08 18:09:09 +0200

    trying to fix non-recurring

--- src/app/[locale]/(protected)/therapist/availability/non-recurring-availability-form.tsx
+++ src/app/[locale]/(protected)/therapist/availability/non-recurring-availability-form.tsx
@@ -115,26 +115,21 @@ const NonRecurringAvailabilityForm = ({
           startDate: string;
           endDate: string;
           appointmentTypeIds: string[];
-        }) => {
-          const formattedStartDate = set(values.date, {
+        }) => ({
+          startDate: set(values.date, {
             hours: parseInt(startDate.split(":")[0], 10),
             minutes: parseInt(startDate.split(":")[1], 10),
             seconds: 0,
             milliseconds: 0,
-          });
-          const formattedEndDate = set(values.date, {
+          }),
+          endDate: set(values.date, {
             hours: parseInt(endDate.split(":")[0], 10),
             minutes: parseInt(endDate.split(":")[1], 10),
             seconds: 0,
             milliseconds: 0,
-          });
-
-          return {
-            startDate: convertToUtcWithTime(formattedStartDate),
-            endDate: convertToUtcWithTime(formattedEndDate),
-            appointmentTypeIds,
-          };
-        }
+          }),
+          appointmentTypeIds,
+        })
       ),
     };
 
