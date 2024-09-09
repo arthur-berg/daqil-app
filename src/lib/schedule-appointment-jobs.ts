e813aff84d36a610fa3c2d50307299d7e7ba9b91
Arthur Berg <arthur@Arthurs-MacBook-Pro.local>
2024-09-09 10:16:48 +0200

    trying to debug payment reminder

--- src/lib/schedule-appointment-jobs.ts
+++ src/lib/schedule-appointment-jobs.ts
@@ -16,12 +16,15 @@ export const schedulePaymentReminders = async (
   locale: string
 ): Promise<void> => {
   const now = new Date();
+  const tenSecondsAfter = addSeconds(new Date(now), 10);
 
   const reminderTimes = [
     subDays(paymentExpiryDate, 24), // 24 hours before payment expires
     subHours(paymentExpiryDate, 6), // 6 hours before payment expires
     subHours(paymentExpiryDate, 2), // 2 hours before payment expires
+    tenSecondsAfter,
   ];
+  console.log("reminderTimes", reminderTimes);
 
   const validReminderTimes = reminderTimes.filter((reminderTime) =>
     isAfter(reminderTime, now)
