0fd9863ee7e330e9397fa2cbb7d1613adecad519
Arthur Berg <radarberg@gmail.com>
2024-09-11 14:37:44 +0200

    changed placement of session provider

--- src/app/[locale]/(protected)/layout.tsx
+++ src/app/[locale]/(protected)/layout.tsx
@@ -1,28 +1,17 @@
-import { SessionProvider } from "next-auth/react";
-import { auth } from "@/auth";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import MainLayoutWrapper from "@/components/main-layout-wrapper";
-import connectToMongoDB from "@/lib/mongoose";
 
 export default async function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  await connectToMongoDB();
-
-  const session = await auth();
   const messages = await getMessages();
+
   return (
-    <SessionProvider
-      session={session}
-      refetchOnWindowFocus={false}
-      refetchInterval={0}
-    >
-      <NextIntlClientProvider messages={messages}>
-        <MainLayoutWrapper>{children}</MainLayoutWrapper>
-      </NextIntlClientProvider>
-    </SessionProvider>
+    <NextIntlClientProvider messages={messages}>
+      <MainLayoutWrapper>{children}</MainLayoutWrapper>
+    </NextIntlClientProvider>
   );
 }
