da04e04c58d47f44370c627e97d0d2d5700301c4
Arthur Berg <radarberg@gmail.com>
2024-09-11 15:27:37 +0200

    moved session checking to login page instaed of parent layout

---
+++ src/app/[locale]/auth/login/loading.tsx
@@ -0,0 +1,16 @@
+import { getTranslations } from "next-intl/server";
+import { BeatLoader } from "react-spinners";
+
+const Loading = async () => {
+  const t = await getTranslations("LoadingPages");
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <BeatLoader color="white" />
+      <div className="text-lg font-medium text-white">
+        {t("loadingLoginForm")}
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,6 +1,18 @@
 import { LoginForm } from "@/components/auth/login-form";
+import connectToMongoDB from "@/lib/mongoose";
+import { auth } from "@/auth";
+import { redirect } from "@/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  await connectToMongoDB();
+
+  const session = await auth();
+
+  console.log("session", session);
+
+  if (!!session) {
+    redirect("/book-appointment");
+  }
   return (
     <div className="h-full flex items-center justify-center container">
       <LoginForm />
