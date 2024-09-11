d785d930402e128a17ac7c7d288d0d06710f0f83
Arthur Berg <radarberg@gmail.com>
2024-09-11 14:41:58 +0200

    added session in other layout back

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,11 +2,8 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { getLangDir } from "rtl-detect";
-import connectToMongoDB from "@/lib/mongoose";
-import { auth } from "@/auth";
 
 import "@/app/globals.css";
-import { SessionProvider } from "next-auth/react";
 
 const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "600"] });
 
@@ -22,23 +19,14 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }>) {
-  await connectToMongoDB();
-
   const direction = getLangDir(locale);
-  const session = await auth();
 
   return (
     <html lang={locale} dir={direction}>
       <body className={dmSans.className}>
         <Toaster />
 
-        <SessionProvider
-          session={session}
-          refetchOnWindowFocus={false}
-          refetchInterval={0}
-        >
-          {children}
-        </SessionProvider>
+        {children}
       </body>
     </html>
   );
