6d23831ce0206a29be15d47a686f888d258d2763
Arthur Berg <arthurwberg@gmail.com>
2024-10-07 15:50:36 +0200

    removed unused code

--- src/app/[locale]/(protected)/(client)/book-appointment/page.tsx
+++ src/app/[locale]/(protected)/(client)/book-appointment/page.tsx
@@ -1,26 +1,15 @@
 import { getAppointmentTypesByIDs } from "@/data/appointment-types";
 import SelectedTherapist from "./selected-therapist";
-import { Button } from "@/components/ui/button";
 import { getClientByIdAppointments, getTherapistById } from "@/data/user";
 import { getCurrentUser } from "@/lib/auth";
-import { Link } from "@/navigation";
 import { getTranslations } from "next-intl/server";
 import {
-  APPOINTMENT_TYPE_ID_INTRO_SESSION,
   APPOINTMENT_TYPE_ID_LONG_SESSION,
   APPOINTMENT_TYPE_ID_SHORT_SESSION,
 } from "@/contants/config";
 import { redirectUserIfReservationExist } from "./helpers";
-import { MdEvent } from "react-icons/md";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import AcceptTherapist from "./accept-therapist";
-import { getFullName } from "@/utils/formatName";
 import connectToMongoDB from "@/lib/mongoose";
-import IntroCallStepManager from "./intro-call-step-manager";
 import PageTitle from "@/components/page-title";
-import Image from "next/image";
-import { format, isWithinInterval } from "date-fns";
-import { formatInTimeZone } from "date-fns-tz";
 import IntroMeetingManager from "./intro-meeting-manager";
 
 const BookAppointmentPage = async ({
