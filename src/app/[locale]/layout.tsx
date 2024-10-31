3f8d60a38f94a82d5e3c16c62886ce849496aa8e
Arthur Berg <arthurwberg@gmail.com>
2024-10-31 13:39:40 +0100

    added gtm script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,8 +8,11 @@ import { GoogleTagManager } from "@next/third-parties/google";
 
 import "@/app/globals.css";
 import TidioChat from "./tidio-chat";
+import Script from "next/script";
 
 const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "600"] });
+
+const GTM_ID = "GTM-WWNR5RVB";
 
 export const metadata: Metadata = {
   title: "Daqil",
@@ -37,8 +40,17 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} dir={direction}>
-      {process.env.NODE_ENV === "production" && (
+      {/* {process.env.NODE_ENV === "production" && (
         <GoogleTagManager gtmId="GTM-WWNR5RVB" />
+      )} */}
+      {process.env.NODE_ENV === "production" && (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
       )}
       <body className={dmSans.className}>
         <Toaster />
