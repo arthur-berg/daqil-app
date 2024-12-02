c21af6cc150ef850377c3589297e43ffb11279b3
arthur-berg <arthurwberg@gmail.com>
2024-12-02 18:38:14 +0100

    updated terms and conditions

--- src/app/[locale]/terms-and-conditions/page.tsx
+++ src/app/[locale]/terms-and-conditions/page.tsx
@@ -44,15 +44,9 @@ const TermsAndConditionsPage = async () => {
             {t("useOfServices.heading")}
           </h2>
           <p>{t("useOfServices.content")}</p>
-
-          <h3 className="text-lg font-semibold mt-4">
-            {t("useOfServices.sessionRecording")}
-          </h3>
+          <br />
           <p>{t("useOfServices.sessionRecording")}</p>
-
-          <h3 className="text-lg font-semibold mt-4">
-            {t("useOfServices.healthDisclaimer")}
-          </h3>
+          <br />
           <p>{t("useOfServices.healthDisclaimer")}</p>
         </section>
 
